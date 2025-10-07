6d987003c301330a55a1fdc39f7f05df1c284969
Nevah5 <contact@nevah5.dev>
2025-10-07 22:21:21 +0200

    Updated entrypoint with optional config path variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,30 @@
 
 CONFIG_PATH="/app/config.yaml"
 CRON_FILE="/etc/cron.d/backup_jobs"
+ENV_FILE="/app/.env"
+
+# Load environment variables
+if [ -f "$ENV_FILE" ]; then
+    export $(grep -v '^#' "$ENV_FILE" | xargs)
+fi
 
 > $CRON_FILE
 
 python3 -c "
 import yaml
-with open('$CONFIG_PATH', 'r') as file:
+import os
+
+CONFIG_PATH = os.getenv('CONFIG_PATH', '/app/config.yaml')
+
+with open(CONFIG_PATH, 'r') as file:
     config = yaml.safe_load(file)
 
 for folder in config['folders']:
     schedule = folder['schedule']
     path = folder['path']
-    print(f'{schedule} python3 /app/backup.py --path {path}')
+
+    command = f'python3 /app/backup.py --path {path}'
+    print(f'{schedule} {command}')
 " >> $CRON_FILE
 
 chmod 0644 $CRON_FILE
